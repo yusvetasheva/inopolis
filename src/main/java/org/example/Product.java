9ce229f8ebb7692392d204aefe4504aeab569ed4
yusvetasheva@digital-spirit.ru <yusvetasheva@digital-spirit.ru>
2024-12-10 16:08:43 +0300

    ДЗ 3

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -2,15 +2,20 @@ package org.example;
 
 import lombok.*;
 
-@Getter
-@Setter
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Product {
     private long id;
     private String desc;
     private int cost;
     private int amount;
+
+    public Product(long id, String desc, int cost, int amount){
+        this.id = id;
+        this.desc = desc;
+        this.cost =cost;
+        this.amount = amount;
+    }
+
+    public Product(){};
 
 }
