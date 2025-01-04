033d14391353b9aef1c748d46eab34e3678c3a72
Svetasheva Yuliya <yusvetasheva@digital-spirit.ru>
2025-01-04 16:58:08 +0300

    Тренировка stream API

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,31 +9,76 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String input = "Hello world! Hello Java. Java is fun.";
-        //[Hello=2, Java=2, is=1, fun=1, world=1]
+        //Задан массив целых чисел от 1 до 100.
+        // В этом массиве есть дубликаты. Задача: найти все дубликаты и указать их количество.
+        // Например 1, 2, 3, 4, 4, 5, 6, 7, 7, 7 4 — 2 штуки, 7 — 3 штуки.
 
-        String [] array = input.replace("!","").replace(".", "").toLowerCase().split(" ");
+        int[] array = new int[]{1, 1, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 8, 8};
 
-        Map<String, Integer> map = Stream.of(array).collect(
-                Collectors.toMap(
-                        i->i,
-                        i->1,
+        //key - num, value - iter
+        Map<Integer, Integer> map = Arrays.stream(array)
+                .boxed()
+                .collect(Collectors.toMap(
+                        i -> i,
+                        i -> 1,
                         Integer::sum
-                )
-        );
+                ));
 
-        // Сортировка по значению в порядке убывания
-        Map<String, Integer> sortedMap  = map.entrySet().stream()
-                        .sorted((e1, e2)->e2.getValue().compareTo(e1.getValue()))
-                                .collect(Collectors.toMap(
-                                        Map.Entry::getKey,
-                                        Map.Entry::getValue,
-                                        (oldValue, newValue)->oldValue,
-                                        LinkedHashMap::new
-                                ));
+        map.forEach((k,v)->System.out.println(k + " = " + v));
 
-        sortedMap.forEach((k,v)->System.out.println(k + " " + v.toString()));
+        map.forEach((k,v)-> {
+            if (v>1){
+                System.out.println(k + " - " + v + " штуки");
+            }
+        });
+
+        Map <Integer, Integer> other = Arrays.stream(array)
+                .boxed()
+                .collect(Collectors.groupingBy(
+                        i->i,
+                        Collectors.summingInt(i->1)
+                ));
+
 
 
     }
+
+    //Сделать первую букву каждого слова заглавной
+    public static void getBigName(String input) {
+        String[] array = input.split(" ");
+        String result = Arrays.stream(array)
+                .map(i -> i.substring(0, 1).toUpperCase() + i.substring(1))
+                .collect(Collectors.joining(" "));
+
+        System.out.println(result);
+    }
+
+    //Задача 2. Напишите функцию, которая принимает на вход массив чисел
+    // и возвращает новый массив c тем же количеством элементов,
+    // где каждый элемент является произведением всех элементов исходного массива, кроме текущего.
+    public static void getProductArrayStreams(double[] numsArray) {
+        double total = Arrays.stream(numsArray).reduce((x, y) -> x * y).getAsDouble();
+        double[] result = Arrays.stream(numsArray).map(i -> total / i).toArray();
+        System.out.println(Arrays.toString(result));
+    }
+
+
+    //Задача 2. Напишите функцию, которая принимает на вход массив чисел
+    // и возвращает новый массив c тем же количеством элементов,
+    // где каждый элемент является произведением всех элементов исходного массива, кроме текущего.
+    public static double[] getClassName(double[] numsArray) {
+        double all = 1;
+        for (int i = 0; i < numsArray.length; i++) {
+            all = numsArray[i] * all;
+        }
+
+        double[] result = new double[numsArray.length];
+
+        for (int i = 0; i < numsArray.length; i++) {
+            result[i] = all / numsArray[i];
+        }
+
+        return result;
+    }
+
 }
