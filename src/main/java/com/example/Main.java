120105e5e478fcc40541b05b5d8004fa3f0b8952
Svetasheva Yuliya <yusvetasheva@digital-spirit.ru>
2025-01-15 21:43:15 +0300

    ReferenceQueue

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,10 +2,7 @@ package com.example;
 
 import lombok.SneakyThrows;
 
-import java.lang.ref.PhantomReference;
-import java.lang.ref.ReferenceQueue;
-import java.lang.ref.SoftReference;
-import java.lang.ref.WeakReference;
+import java.lang.ref.*;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -33,7 +30,8 @@ public class Main {
         System.out.println("weak reference " + weak.get());
 
         /**Fantom
-         * Всегда возвращает null при get*/
+         * Всегда возвращает null при get
+         * то есть через фантомную ссылку нельзя получить доступ к объекту*/
         ReferenceQueue <Object> referenceQueue = new ReferenceQueue<>();
         PhantomReference<Object> phantomReference = new PhantomReference<>(a, referenceQueue);
         System.out.println("phantom reference " + phantomReference.get());
@@ -49,11 +47,31 @@ public class Main {
         System.out.println("phantom reference " + referenceQueue.poll());
 
         //Вызовем gc и убедимся, что он удалит слабую ссылку (мы сделаем попытку вызова, но вызов не гарантирован)
+
+
+        /**Когда объект, на который ссылается слабая, мягкая или фантомная ссылка,
+         * удаляется сборщиком мусора, эта ссылка помещается в очередь ReferenceQueue.
+         Это позволяет разработчику выполнить дополнительные действия, такие
+         как освобождение ресурсов, которые ассоциированы с объектом, до того как объект будет окончательно удален.*/
+        WeakReference<Object> weakRef = new WeakReference<>(a, referenceQueue);
         System.gc();
         System.out.println("После вызова GC");
         System.out.println("weak reference " + weak.get());
 
-
+        // Проверяем, есть ли в очереди ссылки на собранные объекты
+        try {
+            // Получаем ссылку из очереди, если объект был удален
+            Reference<? extends Object> ref = referenceQueue.poll();
+            if (ref == null) {
+                System.out.println("Объект НЕ был удален сборщиком мусора.");
+            } else {
+                System.out.println("Объект БЫЛ удален.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-}
+
+    }
+    
