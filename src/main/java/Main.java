35ad2454bbf1750c27e59ca77f57937d6a9d403f
yusvetasheva@digital-spirit.ru <yusvetasheva@digital-spirit.ru>
2025-01-20 14:00:10 +0300

    Enum Скобки в case не надою Только final - static в case

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,28 @@ public class Main {
         System.out.println(ExampleEnum.PERSON1.ordinal());
 
         System.out.println(ExampleEnum.PERSON1);
+
+        MyEnum someName = MyEnum.NAME1; // Переменная должна быть типа MyEnum
+
+        switch (someName) {
+            case NAME1:
+                System.out.println("case 1");
+                break;
+            case NAME2:
+                System.out.println("case 2");
+                break;
+            default:
+                System.out.println("finally case");
+        }
+
+        int b = 0;
+        int a = switch (b) {
+            case 1 -> 11;
+            case 0 -> 23;
+            default -> 0;
+        };
+        
+        System.out.println(a);
+
     }
 }
--- src/main/java/MyEnum.java
+++ src/main/java/MyEnum.java
@@ -1,12 +1,4 @@
 public enum MyEnum {
-
-    ERX(3),
-    ERC(8);
-    
-    int kol=0;
-
-    private MyEnum(int kol){
-        this.kol=kol;
-    }
-
+    NAME1,
+    NAME2;
 }
