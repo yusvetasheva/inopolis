70973205fdcac7cba60620d333a5345d5e9add1c
Svetasheva Yuliya <yusvetasheva@digital-spirit.ru>
2025-01-29 13:21:45 +0300

    ДЗ для Stream API Код Грея и поиск часто встречающихся слов

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,82 +2,90 @@ package com.example;
 
 import lombok.SneakyThrows;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
     @SneakyThrows
     public static void main(String[] args) {
 
-        //Создаем HashMap. Читаем из файла
-        Map<String, String> map = new HashMap<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("source.txt"))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] array = line.split(", ");
-                for (String i : array) {
-                    String result = i.replaceAll("[{}\"]", "");
-                    String[] mapValue = result.split(":");
-                    map.put(mapValue[0].trim(), mapValue[1].trim());
-                }
-            }
-            map.forEach((k, v) -> System.out.println(k + "=" + v));
-        }
-        System.out.println("=============== a =================");
-
-        //Добавляем 5 значений
-        map.put("new", "111");
-        map.put("orange", "166");
-        map.put("z", "188");
-        map.put("c", "199");
-        map.put("u", "200");
-
-        map.forEach((k, v) -> System.out.println(k + " = " + v));
-        System.out.println("=============== b =================");
-
-        //прямой перебор
-        for (String key : map.keySet())
-            System.out.println(key + " = " + map.get(key));
-        System.out.println("=============== c =================");
-
-        //Добавляем элемент с повторяющимся ключом
-        map.put("z", "0");
-        System.out.println(map.get("z"));
-        System.out.println("=============== d =================");
-
-        //Выносим список ключей в отдельную коллекцию
-        List<String> keyList = map.keySet().stream().toList();
-        keyList.forEach(System.out::println);
-        System.out.println("=============== e =================");
-
-        /**Вынести список всех значений из HashMap в коллекцию HashSet
-         * и получить количество уникальных значений.*/
-        HashSet<String> hashSet = new HashSet<>(map.keySet());
-        hashSet.forEach(System.out::println);
-        System.out.println(hashSet.size());
-        System.out.println("=============== f =================");
-
-        /**Ищем ключ*/
-        System.out.println(map.containsKey("yolo"));
-        System.out.println("=============== g =================");
-
-        /**Ищем значение*/
-        System.out.println(map.containsValue("0"));
-        System.out.println("=============== h =================");
-
-        /**Количество элементов HashMap*/
-        System.out.println(map.size());
-        System.out.println("=============== i =================");
-
-        /**Удаляем элемент по ключу и по значению*/
-        map.remove("raspberry", "10");
-        map.forEach((k, v) -> System.out.println(k + " = " + v));
+        cycleGrayCode(2).limit(10).forEach(System.out::println);
+        readFile(6);
 
     }
+
+    /**
+     * Напишите метод,возвращающий стрим чисел,соответствующих n-битным кодам Грея.
+     */
+    public static Stream<Integer> cycleGrayCode(int n) {
+
+        List<Integer> codes = new ArrayList<>();
+        for (int i = 0; i < (1 << n); i++) {
+            codes.add(i ^ (i >> 1));
+        }
+
+        // Создаем бесконечный поток, который циклично повторяет коды Грея
+        return Stream.generate(new Supplier<Integer>() {
+            private int index = 0;
+
+            @Override
+            public Integer get() {
+                int code = codes.get(index);
+                index = (index + 1) % codes.size();
+                return code;
+            }
+        });
+    }
+
+    /**
+     * Напишите программу, читающую из файла текст в кодировке UTF-8, которая
+     * посчитает в тексте частоту появления слов, и в конце выводит в файл n наиболее часто встречающихся слов.
+     * Решить без циклов и условных операторов
+     */
+
+    @SneakyThrows
+    public static void readFile(int n) {
+        File fileInput = new File("source.txt");
+        File fileOutput = new File("result.txt");
+
+        //слово->количество повторов
+        Map<String, Integer> result = Files.lines(Paths.get(fileInput.getAbsolutePath()))
+                .flatMap(s -> Stream.of(s.split(" ")))
+                .collect(Collectors.toMap(
+                        i -> i,
+                        i -> 1,
+                        Integer::sum
+                ));
+
+
+        //Сортируем папу по убиванию value (частоты повторений слова) и берем первые n элементов
+        List<String> worlds = result.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed() //сортировка по частоте
+                        .thenComparing(Map.Entry.<String, Integer>comparingByKey())) //сортировка по слову
+                .map(Map.Entry::getKey)//взяли слова
+                .limit(n)
+                .toList();
+
+        // Записываем результат в файл с разделителем между словами
+        try (FileWriter fileWriter = new FileWriter(fileOutput)) {
+            worlds.forEach(word -> {
+                        try {
+                            fileWriter.write(word + System.lineSeparator()); // Добавляем перенос строки
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        }
+    }
+
 
 }
