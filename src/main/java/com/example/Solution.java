2b19eb8df042b13d6f340813a359f492c4706479
Svetasheva Yuliya <yusvetasheva@digital-spirit.ru>
2025-02-23 20:26:21 +0300

    Новые заадчи

--- src/main/java/com/example/Solution.java
+++ src/main/java/com/example/Solution.java
@@ -1,35 +1,88 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 class Solution {
 
     public static void main(String[]args){
-        System.out.println(getHappyString(3,9));
+
+        strStr("abc", "ab");
+
+
+
+       // System.out.println(findDifferentBinaryString(new String [] {"00", "01"}));
     }
 
-//    public static String getHappyString(int n, int k) {
-//        List<String> result = new ArrayList<>();
-//        generateHappyStrings(n, "", result);
-//
-//        if (k > result.size()) return ""; // Если K больше количества возможных строк, вернуть ""
-//
-//        return result.get(k - 1); // K-й элемент (1-индексация)
-//    }
-//
-//    private static void generateHappyStrings(int n, String current, List<String> result) {
-//        if (current.length() == n) {
-//            result.add(current);
-//            return;
-//        }
-//
-//        for (char c : new char[]{'a', 'b', 'c'}) {
-//            if (current.isEmpty() || current.charAt(current.length() - 1) != c) {
-//                generateHappyStrings(n, current + c, result);
-//            }
-//        }
-//    }
+    public static int strStr(String haystack, String needle) {
+        String [] big = haystack.split("");
+        String [] small = needle.split("");
+
+        int result =-1;
+        int index =0;
+
+        for (int j=0; j<big.length; j++){
+            if (big[j].equals(small[index])){
+                index++;
+                if (result==-1)
+                    result=j;
+            }
+
+            if (index==small.length) break;
+
+            if (!big[j].equals(small[index])) {
+                result=-1;
+                index=0;
+            }
+        }
+
+        return result;
+
+    }
+
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        List<Integer> list = IntStream.of(nums)
+                .boxed()
+                .toList();
+
+        int currIndex =0;
+        int nextIndex = -1;
+
+        for (Integer i : list){
+            if (list.contains(target-i)){
+                nextIndex = list.indexOf(target-i);
+                break;
+            }
+            currIndex++;
+        }
+
+        return new int [] {currIndex, nextIndex};
+    }
+
+    public static String findDifferentBinaryString(String[] nums) {
+        Set<String> exist = new HashSet<>(List.of(nums));
+        return generate(exist, "", nums[0].length());
+    }
+
+    private static String generate(Set<String> exist, String current, int max) {
+        if (current.length() == max) {
+            if (!exist.contains(current)) {
+                return current;
+            }
+            return null;
+        }
+
+        String res = generate(exist, current + "0", max);
+        if (res != null) return res; // Если найден результат, сразу возвращаем
+
+        res = generate(exist, current + "1", max);
+        return res; // Если найдено в правой ветке, возвращаем
+    }
 
     public static String getHappyString(int n, int k) {
 
