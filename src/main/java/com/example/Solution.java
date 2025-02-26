a46a7fe6eeabecf3396f3a3c29cbef917b0cba9e
Svetasheva Yuliya <yusvetasheva@digital-spirit.ru>
2025-02-26 21:16:22 +0300

    Новые задачи

--- src/main/java/com/example/Solution.java
+++ src/main/java/com/example/Solution.java
@@ -1,41 +1,113 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 class Solution {
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
-        strStr("abc", "ab");
+  Integer a = (int) Math.pow(10,2);
+        //return sum;
+    }
 
+    /**
+     * Перевернуть массив.
+     */
 
+    public static void reverseArray() {
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 5};
+        int tenp = -1;
+        int index = array.length - 1;
+        for (int i = 0; i < array.length / 2; i++) {
+            tenp = array[i];
+            array[i] = array[index];
+            array[index] = tenp;
+            index--;
+        }
 
-       // System.out.println(findDifferentBinaryString(new String [] {"00", "01"}));
+        IntStream.of(array).forEach(System.out::println);
+
+    }
+
+    /**
+     * Дан целочисленный массив nums. Необходимо переместить все нулевые элементы в конец массива,
+     * сохраняя относительный порядок элементов, не являющихся нулем.
+     * Решение должно производиться на месте, без использования дополнительного массива,
+     * а также должно иметь минимальную сложность по времени и пространству.
+     */
+
+    public void moveZeroes(int[] nums) {
+
+        if (nums == null || nums.length == 0) return;
+
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+    }
+
+    /**
+     * Given List names. Удалите первую букву из каждого имени и поверните отсортированный список.
+     */
+
+    public static void getNanes(List<String> input) {
+        if (input == null || input.isEmpty()) return;
+
+        List<String> result = input.stream()
+                .map(i -> i.substring(1))
+                .sorted(Comparator.reverseOrder())
+                .toList();
+
+        result.forEach(System.out::println);
+    }
+
+    /**
+     * Дана квадратная матрица. Найти сумму элементов на ее диагонали.
+     */
+
+    public static int diagonalSum(int[][] matrix) {
+
+        if (matrix == null || matrix.length == 0) return 0;
+
+        int sum = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == j)
+                    sum += matrix[i][j];
+            }
+        }
+
+        return sum;
+
     }
 
     public static int strStr(String haystack, String needle) {
-        String [] big = haystack.split("");
-        String [] small = needle.split("");
+        String[] big = haystack.split("");
+        String[] small = needle.split("");
 
-        int result =-1;
-        int index =0;
+        int result = -1;
+        int index = 0;
 
-        for (int j=0; j<big.length; j++){
-            if (big[j].equals(small[index])){
+        for (int j = 0; j < big.length; j++) {
+            if (big[j].equals(small[index])) {
                 index++;
-                if (result==-1)
-                    result=j;
+                if (result == -1)
+                    result = j;
             }
 
-            if (index==small.length) break;
+            if (index == small.length) break;
 
             if (!big[j].equals(small[index])) {
-                result=-1;
-                index=0;
+                result = -1;
+                index = 0;
             }
         }
 
@@ -50,18 +122,18 @@ class Solution {
                 .boxed()
                 .toList();
 
-        int currIndex =0;
+        int currIndex = 0;
         int nextIndex = -1;
 
-        for (Integer i : list){
-            if (list.contains(target-i)){
-                nextIndex = list.indexOf(target-i);
+        for (Integer i : list) {
+            if (list.contains(target - i)) {
+                nextIndex = list.indexOf(target - i);
                 break;
             }
             currIndex++;
         }
 
-        return new int [] {currIndex, nextIndex};
+        return new int[]{currIndex, nextIndex};
     }
 
     public static String findDifferentBinaryString(String[] nums) {
@@ -92,23 +164,23 @@ class Solution {
         getMyString("b", result, n);
         getMyString("c", result, n);
 
-        if (result.size()>=k)
-            return result.get(k-1);
+        if (result.size() >= k)
+            return result.get(k - 1);
         else return "";
 
     }
 
-    public static void getMyString (String curr, List<String> result, int max){
-        if (curr.length()==max){
+    public static void getMyString(String curr, List<String> result, int max) {
+        if (curr.length() == max) {
             result.add(curr);
-            curr="";
+            curr = "";
             return;
         }
 
         int len = curr.length();
 
-        String [] array = curr.split("");
-        String last = array[len-1];
+        String[] array = curr.split("");
+        String last = array[len - 1];
 
         // Если последний символ "a"
         if (last.equals("a")) {
