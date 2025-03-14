16a10396edb41cebf91ca144a18b1bdfa3c5b72c
Svetasheva Yuliya <yusvetasheva@digital-spirit.ru>
2025-03-14 10:40:37 +0300

    Добавлено решение новых задач

--- src/main/java/com/example/Solution.java
+++ src/main/java/com/example/Solution.java
@@ -8,47 +8,75 @@ class Solution {
 
     public static void main(String[] args) {
 
-        numberOfAlternatingGroups(new int [] {0,1,1}, 3);
+        int[] arr = new int[]{1, 2, 3, 4, 2, 3, 7, 8};
+
+        int unic = 0;
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++)
+            set.add(arr[i]);
+
+        unic= set.size();
+
+
+        int result = 0;
+
+        while(unic>0){
+            result+=fact(unic);
+            unic--;
+        }
+
+        System.out.println(unic);
+
+
     }
 
+    public static int fact(int input) {
+        if (input > 1) return input * fact(input - 1);
+        return input;
+    }
+
+    //Дан int[]. Найти число подмассивов, где все значения всех чисел разные
+
+
     public static int numberOfAlternatingGroups(int[] colors, int k) {
-        if (colors.length<k) return 0;
+        if (colors.length < k) return 0;
         List<Integer> result = new ArrayList<>();
 
-        int currLen =1;
+        int currLen = 1;
         int prev = colors[0];
 
-        for (int i =1; i<colors.length; i++){
-            if (colors[i]!=prev){
-                prev=colors[i];
+        for (int i = 1; i < colors.length; i++) {
+            if (colors[i] != prev) {
+                prev = colors[i];
                 currLen++;
-            }
-            else{
+            } else {
                 result.add(currLen);
-                currLen=1;
+                currLen = 1;
             }
         }
 
-        if (currLen>1) result.add(currLen);
+        if (currLen > 1) result.add(currLen);
 
-        if (result.size()==0) return 0;
+        if (result.size() == 0) return 0;
 
         int size = result.size();
 
-        if (colors[0]!=colors[colors.length-1]){
-            Integer sum  = 0;
-            if (result.size()==1) sum = result.get(0)+1;
-            else sum = result.get(0) + result.get(size-1);
+        if (colors[0] != colors[colors.length - 1]) {
+            Integer sum = 0;
+            if (result.size() == 1) sum = result.get(0) + 1;
+            else sum = result.get(0) + result.get(size - 1);
             result.set(0, sum);
-            result.remove(size-1);
+            result.remove(size - 1);
         }
 
-        int out =0;
+        int out = 0;
 
-        for (Integer i : result){
-            if (i==k) out++;
-            else if (i>k){
-                out+=i-k+1;
+        for (Integer i : result) {
+            if (i == k) out++;
+            else if (i > k) {
+                out += i - k + 1;
             }
         }
 
@@ -57,27 +85,27 @@ class Solution {
 
     public static int[] plusOne(int[] digits) {
 
-        int perenos =1;
-        int last =0;
+        int perenos = 1;
+        int last = 0;
 
-        if (digits[digits.length-1]!=9){
-            digits[digits.length-1] = digits[digits.length-1]+1;
+        if (digits[digits.length - 1] != 9) {
+            digits[digits.length - 1] = digits[digits.length - 1] + 1;
             return digits;
         }
 
-        for(int i = digits.length-1; i>=0; i--){
-            last = digits[i]+perenos;
-            perenos = last/10;
-            digits[i] = last%10;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            last = digits[i] + perenos;
+            perenos = last / 10;
+            digits[i] = last % 10;
 
-            if (perenos==0) return digits;
+            if (perenos == 0) return digits;
         }
 
-        int [] result = new int [digits.length];
+        int[] result = new int[digits.length];
         result[0] = perenos;
 
-        for (int i=1; i<digits.length; i++){
-            result[i] = digits[i-1];
+        for (int i = 1; i < digits.length; i++) {
+            result[i] = digits[i - 1];
         }
         return result;
     }
@@ -87,9 +115,9 @@ class Solution {
 
         StringBuilder openSign = new StringBuilder();
 
-        String[]array = s.split("");
+        String[] array = s.split("");
 
-        for (int i=0; i<array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             if (array[i].equals("(") ||
                     array[i].equals("{") ||
                     array[i].equals("[")) {
@@ -105,10 +133,10 @@ class Solution {
             if (!array[i].equals(reverse))
                 return false;
 
-            openSign.deleteCharAt(len-1);
+            openSign.deleteCharAt(len - 1);
         }
 
-        if (openSign.length()==0) return true;
+        if (openSign.length() == 0) return true;
         return false;
 
     }
