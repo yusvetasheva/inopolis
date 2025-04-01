a97bcb39bc3030723a00b6dac6bce5a3bde6fa3e
Svetasheva Yuliya <yusvetasheva@digital-spirit.ru>
2025-04-01 17:22:36 +0300

    Дорабротка задачи

--- src/main/java/com/example/inopolis/Solution.java
+++ src/main/java/com/example/inopolis/Solution.java
@@ -1,38 +1,35 @@
 package com.example.inopolis;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Нужнно удалить из списка анаграммы, в итоговом списке анаграмм быть не должно
+ */
 public class Solution {
     public static void main(String[] args) {
-        List anagrams = List.of("Race", "NighT", "Angle", "CaRe", "angel", "ThiNG", "agnel");
+        List<String> anagrams = List.of("Race", "NighT", "Angle", "CaRe", "angel", "ThiNG", "agnel");
 
         System.out.println(removeAnagrams(anagrams));
     }
 
 
-    static List removeAnagrams(List anagrams) {
+    static List<String> removeAnagrams(List<String> anagrams) {
+        //new sorted value, old init value
+        HashMap<String, String> map = new HashMap<>();
+        String sorted = "";
 
+        for (String i : anagrams) {
+            char[] array = i.toLowerCase().toCharArray();
+            Arrays.sort(array);
+            sorted = new String(array);
 
-
-    }
-
-    static boolean isAnagram(String first, String second){
-        if (first.length()!=second.length()) return false;
-
-        int [] array = new int [26];
-
-        for (char c : first.toCharArray()){
-            array[Character.toLowerCase(c) -'a']++;
+            map.putIfAbsent(sorted, i);
         }
 
-        for (char c : second.toCharArray()){
-            array[Character.toLowerCase(c) -'a']--;
-        }
+        return new ArrayList<>(map.values());
 
-        for (int i : array){
-            if (i!=0) return false;
-        }
-
-        return true;
     }
 }
